74e65cca63aae9fda82d9ed5edfcd6b9ec955157
vasilevski85@gmail.com <vasilevski85@gmail.com>
2019-06-19 18:37:58 +0200

    updated dashboard cookie, updated auth middleware

--- public/javascripts/dashboard.js
+++ public/javascripts/dashboard.js
@@ -67,8 +67,8 @@ function loginAjax(formData){
         //Dismiss modal
         $('#login-modal').modal('toggle');
         alertMessage('Login successfuly', 'alert-success', 3000);
-        var token = response.token;
-        console.log(token)
+        //Create cookie
+        createCookie("jwt", response.token, 7);
     },
     error: function (err){
         console.log(err);
@@ -239,4 +239,58 @@ function addResultsToList(results){
       tr.appendChild(tdDel);
       tbody.appendChild(tr);
   }
+}
+
+ /* Decode token info */
+ function getTokenInfo(ca){
+  var base64Url = ca.split('.')[1];
+  var decodedValue = JSON.parse(window.atob(base64Url));
+  return decodedValue;
+}
+
+
+/* Check if token is valid */
+function isValidToken(token){
+  if (Date.now() / 1000 > getTokenInfo(token).exp) {
+      return false;
+  }else{
+      return true;
+  }
+}
+
+
+// function createCookie(key, value){
+//   var cookie = escape(key) + '=' + escape(value) + ';';
+//   document.cookie = cookie;
+//   console.log('Creating cookie with key: ' + key + ' value: ' + value);
+// }
+
+function createCookie(name,value,exdays) {
+  var exdate=new Date();
+  exdate.setDate(exdate.getDate() + exdays);
+  document.cookie=encodeURIComponent(name) 
+    + "=" + encodeURIComponent(value)
+    + (!exdays ? "" : "; expires="+exdate.toUTCString());
+    ;
+}
+
+
+function getCookie(cname) {
+  var name = cname + "=";
+  var ca = document.cookie.split(';');
+  for(var i = 0; i < ca.length; i++) {
+    var c = ca[i];
+    while (c.charAt(0) == ' ') {
+      c = c.substring(1);
+    }
+    if (c.indexOf(name) == 0) {
+      return c.substring(name.length, c.length);
+    }
+  }
+  return "";
+}
+
+
+function deleteCookie(name){
+  document.cookie = name + '=; expires=Thu, 01 Jan 1970 00:00:01 GMT;';
 }
